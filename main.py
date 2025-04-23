6325e7c667bc33e20bb870e4e742a379caccdcdb
testbotKIM <testbotkim@gmail.com>
2025-04-23 18:43:54 +0900

    feat: Add DrawerSampling funcs

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ def main():
        and 100 prisoners, over 100,000 trial runs.
     '''
     halfOfDrawers = randomRInt(0)(1)
+
+    def optimalDrawerSampling(x):
+        return allChainedPathsAreShort(x)
+
+    def randomDrawerSampling(x):
+        return randomTrialResult(halfOfDrawers)(x)
